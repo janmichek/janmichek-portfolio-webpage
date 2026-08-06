55fcfc716e1b9c6f75c246809b538b79ea16c743
Jan Michek <jan@janmichek.cz>
2026-08-06 08:01:52 +0200

    move content

--- components/SectionSummary.vue
+++ components/SectionSummary.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { portfolio } from '~/data/portfolio'
-</script>
-
 <template>
   <section class="summary">
     <div class="u-container">
@@ -13,7 +9,10 @@ import { portfolio } from '~/data/portfolio'
       <p
         class="summary__text"
         data-aos="fade-up">
-        {{ portfolio.summary }}
+        Senior engineer with 14 years of experience, including 5+ years of building Web3 experiences on smart contracts.
+        My background includes leading the development of aescan.io, an open-source blockchain explorer, and crafting
+        smooth Web3 UX flows for an NFT lending platform at PWN. I am proud of winning an award at the Polkadot
+        Hackathon. I combine full-stack expertise in JS frameworks with Solidity to build dApps that are a joy to use.
       </p>
     </div>
   </section>
